6b1caa2c1efa2f12631c7d15a87e41854cd6d41b
Andy Truong <thehongtt@gmail.com>
2014-06-12 14:34:06 +0700

    Fix Angular app bootstrap issue.

--- misc/gogridster.js
+++ misc/gogridster.js
@@ -1,4 +1,5 @@
-(function($, angular) {
+(function($, angular, Drupal) {
+  var boot = false;
 
   var do_drag_resize = function(status) {
     for (var app_name in Drupal.settings.gridster) {
@@ -8,11 +9,13 @@
         var div_id = $('div[ng-app="'+ app_name +'"]').attr('id');
         $('#' + div_id).scope().$digest();
       }
-      catch (e) {}
+      catch (e) {
+
+      }
     }
   };
 
-  Drupal.behaviors.gridsterGoInit_ = {
+  Drupal.behaviors.gridsterGo = {
     attach: function(context, settings) {
       do_drag_resize($('#panels-ipe-save').size() ? true : false);
     }
@@ -20,12 +23,16 @@
 
   Drupal.behaviors.gridsterGoInit = {
     attach: function(context, settings) {
-      var firstModule = 0;
-      var initAngular = function(app_id, settings) {
-        angular.module(app_id, ['goGridster'])
+      var start = function(app_id, div_id, settings, boot) {
+        console.log([app_id, div_id, settings, boot]);
+
+        angular
+          .module(app_id, ['goGridster'])
           .controller('gridsterCtrl', ['$scope', function($scope) {
             $scope[app_id] = settings.gridster[app_id];
           }]);
+
+        boot && angular.bootstrap($('#' + div_id).get(0), [app_name]);
       }
 
       for (var app_name in settings.gridster) {
@@ -33,17 +40,11 @@
 
         $('#' + div_id, context).once('gridsterGoInit').each(function(){
           $(this).append('<div grid-master="'+ app_name +'"></div>');
-
-          initAngular(app_name, settings);
-
-          if (firstModule >= 1) {
-            angular.bootstrap($('#' + div_id).get(0), [app_name]);
-          }
-
-          firstModule++;
+          start(app_name, div_id, settings, boot);
+          boot = true;
         });
       }
     }
   };
 
-})(jQuery, angular);
+})(jQuery, angular, Drupal);
